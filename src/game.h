7ecfa92c771082647e1b4c1eed7ce118245edf52
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2015-01-12 17:56:37 +0100

    Jetzt sollten die letzten Änderungen wieder kompilieren.

--- src/game.h
+++ src/game.h
@@ -3,25 +3,44 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <sys/time.h>
 
 #include "image.h"
 #include "font.h"
 #include "view.h"
+#include "model.h"
+#include "controller.h"
+#include "config.h"
 
 #define PLAYER_LEFT 0
 #define PLAYER_RIGHT 1
 
+#define CONTROL_UP 0x01
+#define CONTROL_DOWN 0x02
+#define CONTROL_LEFT 0x04
+#define CONTROL_RIGHT 0x08
+#define CONTROL_W 0x10
+#define CONTROL_S 0x20
+
+enum model_state;
+
 struct game_state {
-    SDL_Window *window;
-    SDL_Surface *screen;
-    SDL_Surface *images[IMAGE_NUMBER];
-    TTF_Font *fonts[FONT_NUMBER];
-    unsigned int position[2];
-    unsigned int score[2];
-    const char *name[2];
-    unsigned int ball_position[2];
-    int ball_speed[2];
-    unsigned char quit;
+    SDL_Window *window; // nur vom View genutzt
+    SDL_Surface *screen; // nur vom View genutzt
+    SDL_Surface *images[IMAGE_NUMBER]; // nur vom View genutzt
+    TTF_Font *fonts[FONT_NUMBER]; // nur vom View genutzt
+    const char *name[2]; // lesen: Model und View, schreiben: Model
+    unsigned int score[2]; // lesen: Model und View, schreiben: Model
+    double position[2]; // lesen: Model und View, schreiben: Model
+    double speed[2]; // nur vom Model genutzt
+    double ball_position[2]; // lesen: Model und View, schreiben: Model
+    double ball_speed[2]; // nur vom Model genutzt
+    enum model_state state; // nur vom Model genutzt
+    struct timeval last_time; // nur vom Model genutzt
+    unsigned char quit; // lesen: Model, schreiben: Controller
+    unsigned char controls; // lesen: Model, schreiben: Controller
+    unsigned int resolution[2]; // lesen: View und Config, schreiben: Config und Menu
+    unsigned char fullscreen; // lesen: View und Config, schreiben: Config und Menu
 };
 
 #endif
--- src/main.c
+++ src/main.c
@@ -7,9 +7,19 @@ static struct game_state the_game;
 int main(void)
 {
     memset(&the_game, 0, sizeof(the_game));
-    if (view_init(&the_game) < 0) {
+    config_read(&the_game);
+    if (model_init(&the_game) < 0) {
         return -1;
     }
+    if (view_init(&the_game) < 0) {
+        model_destroy(&the_game);
+        return -1;
+    }
+    while (!(the_game.quit)) {
+        controller_cycle(&the_game);
+    }
     view_destroy(&the_game);
+    model_destroy(&the_game);
+    config_write(&the_game);
     return 0;
 }
